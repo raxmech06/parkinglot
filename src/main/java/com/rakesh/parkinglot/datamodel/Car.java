37d26eeaaab121173f09dd410e2f86453d7880d4
raxmech06 <raxmech06@gmail.com>
2019-05-26 17:41:00 +0530

    Create Car.java
    Car is one concrete implementation of Vehicle

---
+++ src/main/java/com/rakesh/parkinglot/datamodel/Car.java
@@ -0,0 +1,7 @@
+package com.rakesh.parkinglot.datamodel;
+
+public class Car extends Vehicle {
+    public Car(String registrationNumber, String color) {
+        super(registrationNumber, color);
+    }
+}
