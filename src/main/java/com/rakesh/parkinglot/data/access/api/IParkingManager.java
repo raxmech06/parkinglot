a8a412704443b7bb2cfa0a57110c926da11c64c5
raxmech06 <raxmech06@gmail.com>
2019-05-26 22:31:49 +0530

    Create IParkingManager.java
    Overall parking management

---
+++ src/main/java/com/rakesh/parkinglot/data/access/api/IParkingManager.java
@@ -0,0 +1,23 @@
+package com.rakesh.parkinglot.data.access.api;
+
+import com.rakesh.parkinglot.datamodel.Vehicle;
+
+import java.util.List;
+
+public interface IParkingManager <T extends Vehicle> {
+    public int parkCar(int level, T vehicle);
+
+    public boolean leaveCar(int level, int slotNumber);
+
+    public List<String> getStatus(int level);
+
+    public List<String> getRegistrationNumbersForColor(int level, String color);
+
+    public List<Integer> getSlotNumbersFromColor(int level, String colour);
+
+    public int getSlotNoFromRegistrationNumber(int level, String registrationNo);
+
+    public int getAvailableSlotsCount(int level);
+
+    public void doSystemCleanup();
+}
