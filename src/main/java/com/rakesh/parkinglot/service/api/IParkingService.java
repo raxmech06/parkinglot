748fa19733b188d6c32fa89879bbfc0266880612
raxmech06 <raxmech06@gmail.com>
2019-05-26 17:34:37 +0530

    Creating parking service high level api
    This parking service can be easily extended for multi-level parking

---
+++ src/main/java/com/rakesh/parkinglot/service/api/IParkingService.java
@@ -0,0 +1,25 @@
+package com.rakesh.parkinglot.service.api;
+
+import com.rakesh.parkinglot.datamodel.Vehicle;
+
+import java.util.Optional;
+
+public interface IParkingService {
+    public void createParkingLot(int level, int capacity);
+
+    public Optional<Integer> park(int level, Vehicle vehicle);
+
+    public void unPark(int level, int slotNumber);
+
+    public void getStatus(int level);
+
+    public Optional<Integer> getAvailableSlotsCount(int level);
+
+    public void getRegNumberForColor(int level, String color);
+
+    public void getSlotNumbersFromColor(int level, String colour);
+
+    public int getSlotNoFromRegistrationNumber(int level, String registrationNumber);
+
+    public void doSystemCleanup();
+}
