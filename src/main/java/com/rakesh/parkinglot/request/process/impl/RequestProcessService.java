0b883f3b1438c0c437b99d679a207ec0b118efaa
raxmech06 <raxmech06@gmail.com>
2019-05-26 21:56:24 +0530

    Create RequestProcessService.java
    Implementation of the request process service.

---
+++ src/main/java/com/rakesh/parkinglot/request/process/impl/RequestProcessService.java
@@ -0,0 +1,45 @@
+package com.rakesh.parkinglot.request.process.impl;
+
+import com.rakesh.parkinglot.datamodel.Car;
+import com.rakesh.parkinglot.request.process.api.IRequestProcessService;
+import com.rakesh.parkinglot.service.api.IParkingService;
+import com.rakesh.parkinglot.util.ParkingConstantUtil;
+
+public class RequestProcessService implements IRequestProcessService {
+    private IParkingService parkingService;
+    @Override
+    public void setParkingService(IParkingService parkingService) {
+        this.parkingService = parkingService;
+    }
+
+    @Override
+    public void executeRequest(String request) {
+        int level = 0; //Tomorrow this parking lot can be extended to a multi storey parking.
+        String[] requestParts = request.split(" ");
+        String keyAction = requestParts[0];
+        switch (keyAction){
+            case ParkingConstantUtil.CREATE_PARKING_LOT :
+                int maxCapacity = Integer.parseInt(requestParts[1]);
+                parkingService.createParkingLot(level, maxCapacity);
+                break;
+            case ParkingConstantUtil.PARK :
+                parkingService.park(level, new Car(requestParts[1], requestParts[2]));
+                break;
+            case ParkingConstantUtil.LEAVE :
+                int parkingSlotNumber = Integer.parseInt(requestParts[1]);
+                parkingService.unPark(level, parkingSlotNumber);
+                break;
+            case ParkingConstantUtil.REG_NUMBER_FOR_CARS_WITH_COLOR :
+                parkingService.getRegNumberForColor(level, requestParts[1]);
+                break;
+            case ParkingConstantUtil.SLOTS_NUMBER_FOR_CARS_WITH_COLOR:
+                parkingService.getSlotNumbersFromColor(level, requestParts[1]);
+                break;
+            case ParkingConstantUtil.SLOTS_NUMBER_FOR_REG_NUMBER:
+                parkingService.getSlotNoFromRegistrationNumber(level, requestParts[1]);
+                break;
+            default:
+                break;
+        }
+    }
+}
