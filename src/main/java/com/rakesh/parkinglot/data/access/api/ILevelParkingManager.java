bb51c780e0a9830a163613bc5413525f359b2c69
raxmech06 <raxmech06@gmail.com>
2019-05-26 22:34:18 +0530

    Create ILevelParkingManager.java
    Overall parking management at a particular level for the multi-storeyed parking lot.

---
+++ src/main/java/com/rakesh/parkinglot/data/access/api/ILevelParkingManager.java
@@ -0,0 +1,23 @@
+package com.rakesh.parkinglot.data.access.api;
+
+import com.rakesh.parkinglot.datamodel.Vehicle;
+
+import java.util.List;
+
+public interface ILevelParkingManager<T extends Vehicle> {
+    public int parkCar(T vehicle);
+
+    public boolean leaveCar(int slotNumber);
+
+    public List<String> getStatus();
+
+    public List<String> getRegistrationNumbersForColor(String color);
+
+    public List<Integer> getSlotNumbersFromColor(String colour);
+
+    public int getSlotNoFromRegistrationNumber(String registrationNo);
+
+    public int getAvailableSlotsCount();
+
+    public void doSystemCleanup();
+}
