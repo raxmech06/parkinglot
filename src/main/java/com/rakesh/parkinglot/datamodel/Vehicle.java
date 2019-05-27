90cb52574df844dc5a43f55b2cdc0dfcf34d2009
raxmech06 <raxmech06@gmail.com>
2019-05-27 07:52:28 +0530

    Update Vehicle.java
    Adding setters and getters

--- src/main/java/com/rakesh/parkinglot/datamodel/Vehicle.java
+++ src/main/java/com/rakesh/parkinglot/datamodel/Vehicle.java
@@ -3,8 +3,33 @@ package com.rakesh.parkinglot.datamodel;
 public abstract class Vehicle {
     private String registrationNumber;
     private String color;
+
     public Vehicle(String registrationNumber, String color) {
         this.registrationNumber = registrationNumber;
         this.color = color;
+    }
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Vehicle{" +
+                "registrationNumber='" + registrationNumber + '\'' +
+                ", color='" + color + '\'' +
+                '}';
     }
 }
