be367ae03d03ed4a30d0fb1a1c1805d51221207f
raxmech06 <raxmech06@gmail.com>
2019-05-27 06:23:25 +0530

    Update ParkingService.java

--- src/main/java/com/rakesh/parkinglot/service/impl/ParkingService.java
+++ src/main/java/com/rakesh/parkinglot/service/impl/ParkingService.java
@@ -1,14 +1,35 @@
 package com.rakesh.parkinglot.service.impl;
 
+import com.rakesh.parkinglot.data.access.api.IParkingManager;
+import com.rakesh.parkinglot.data.access.impl.ParkingManager;
 import com.rakesh.parkinglot.datamodel.Vehicle;
+import com.rakesh.parkinglot.exception.ParkingException;
+import com.rakesh.parkinglot.parkingstrategy.api.VehicleParkingStrategy;
+import com.rakesh.parkinglot.parkingstrategy.impl.ShortestDisFirstParkingStrategy;
 import com.rakesh.parkinglot.service.api.IParkingService;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public class ParkingService implements IParkingService {
-    @Override
-    public void createParkingLot(int level, int capacity) {
+    private IParkingManager<Vehicle> parkingManager = null;
+    private ReentrantReadWriteLock lock = new ReentrantReadWriteLock();
 
+    @Override
+    public void createParkingLot(int level, int capacity) throws ParkingException {
+        if (parkingManager != null) {
+            throw new ParkingException(ParkingException.ErrorCode.PARKING_ALREADY_EXIST.getMessage());
+        }
+        List<Integer> parkingLevelsList = new ArrayList<>();
+        parkingLevelsList.add(level);
+        List<Integer> capacityList = new ArrayList<>();
+        capacityList.add(capacity);
+        List<VehicleParkingStrategy> vehicleParkingStrategiesList = new ArrayList<>();
+        vehicleParkingStrategiesList.add(new ShortestDisFirstParkingStrategy());
+        parkingManager = ParkingManager.getInstance(parkingLevelsList, capacityList, vehicleParkingStrategiesList);
+        System.out.println("Created a parking lot with " + capacity + " 6 slots");
     }
 
     @Override
