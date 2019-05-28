31a59dbfe59e2d677c8ba70570e9b35036ea29ee
raxmech06 <raxmech06@gmail.com>
2019-05-28 19:22:17 +0530

    Update parking_lot.sh

--- bin/parking_lot.sh
+++ bin/parking_lot.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-arg1=./src/main/resources/file_inputs.txt
+arg1=$1
+#arg1=./src/main/resources/file_inputs.txt
 dir=./target
 jar_name=parkinglot-1.0-SNAPSHOT.jar
 java -jar $dir/$jar_name $arg1
